74705bf7768f6f6fbaa06fed6e3af1cbfdb88d44
drewparker <drewjparker91@gmail.com>
2020-10-30 10:42:35 -0700

    add excepcion rb file in concerns folder, indlude Excepcion model in application_controller.rb

---
+++ app/controllers/concerns/excepcion.rb
@@ -0,0 +1,20 @@
+require 'active_support/concern'
+
+module Excepcion
+  extend ActiveSupport::Concern
+
+  included do
+    
+    rescue_from ActiveRecord::RecordNotFound do |exception|
+      json_response({ message: exception.message }, 404)
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |exception|
+      json_response({ message: exception.message }, 422)
+    end
+
+    rescue_from TypeError do |exception|
+      json_response({ message: exception.message }, 500)
+    end
+  end
+end
