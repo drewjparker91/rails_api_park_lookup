a3051ab2aaa4d938be3f55c4d25031a31b22cbf7
drewparker <drewjparker91@gmail.com>
2020-10-30 09:52:57 -0700

    add validations for presence of name, location, review, rating. Validate rating as integer between 1 and 5, add titleize function and callback

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,2 +1,11 @@
 class Park < ApplicationRecord
+  validates :name, :location, :review, :rating, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  
+
+private
+  def titleize
+    self.name = self.name.titleize
+    self.location = self.location.titleize
+  end
 end
